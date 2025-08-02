f073c1b3e9f1a5d272722f78414f2e1cc1d7ad95
Bahar Barghbani <baharbarghbani@Bahars-MacBook-Air.local>
2025-08-02 23:30:25 +0330

    working on posts

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     def __str__ (self):
-        return self.name
-    post_name = models.CharField(max_length=20, default="Post")
-    post_preview = models.CharField(max_length=100, default="Preview")
+        return self.post_title
+    post_title = models.CharField(max_length=20, default="Post")
+    post_preview = models.CharField(max_length=200, default="Preview")
     content = models.CharField(max_length=200)
-    title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='post_images/', blank=True, null=True) # needs edit
