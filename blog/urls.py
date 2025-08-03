cc7d3e854ed91279e56502df14cc24ce50ca8548
Bahar Barghbani <baharbarghbani@Bahars-MacBook-Air.local>
2025-08-03 16:34:53 +0330

    made posts clikable

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path("posts/", views.Posts, name="posts"),
+    path("posts/<int:post_id>/", views.PostDetail, name="detail"),
 ] 
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,10 @@ def Posts(request):
         'posts_list': posts,
     }
     return render(request, 'blog/posts.html', context)
+
+def PostDetail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        "post": post
+    }
+    return render(request, "blog/post_detail.html", context)
