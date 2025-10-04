d3bbd5e7cbdf8ba26a393d7193c68daca3b22c36
Bahar Barghbani <baharbarghbani@Bahars-MacBook-Air.local>
2025-10-04 19:44:08 +0330

    added no comment and comment text area ui design

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class CommentForm(forms.Form):
-    body = forms.CharField(max_length=256, label="Your Comment")\
+    body = forms.CharField(max_length=255, label="", widget=forms.Textarea(attrs={'placeholder': 'Write your comment here...', 'class': 'comment-input'}))
     
