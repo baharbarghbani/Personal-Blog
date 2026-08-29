7ca6b27b4f3fee8bc5b1e922ef4c652cd0a640e7
baharbarghbani <bahar.brqbni@gmail.com>
2026-08-29 19:51:15 +0330

    Update home and logout routes

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'pages'
 urlpatterns = [
-    path("home/", views.HomePage, name='home'),
+    path("", views.HomePage, name='home'),
     path("about/", views.About, name='about'),
     path("contact/", views.Contact, name='contact'),
     path("thank_you/", views.Thankyou, name='thank-you'),
